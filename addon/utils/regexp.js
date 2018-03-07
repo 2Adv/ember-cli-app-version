b57e1c2658a6f02047d2d1d1de36ede46481580b
Marcelo Fernandes <marcelofernandes.mga@gmail.com>
2018-03-07 09:13:38 -0300

    Update regexp.js
    semVersionRegExp

--- addon/utils/regexp.js
+++ addon/utils/regexp.js
@@ -1,2 +1,3 @@
 export const versionRegExp = /\d[.]\d[.]\d/;
 export const shaRegExp = /[a-z\d]{8}/;
+export const semVersionRegExp = /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/g;
